b67d9dab7339951f8ae17a1f0f4879cdad056e0c
irinkwink <irina-ns@yandex.ru>
2022-09-25 14:18:41 +0300

    day 3

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,10 +1,14 @@
 const bgElements = () => {
   const elements = document.querySelectorAll('.set-bg');
 
-  for (let i = 0; i < elements.length; i++) {
-    const src = elements[i].dataset.setbg;
-    elements[i].style.backgroundImage = `url('${src}')`;
-  }
+  elements.forEach(item => 
+    item.style.backgroundImage = `url('${item.dataset.setbg}')`
+  )
+
+  // for (let i = 0; i < elements.length; i++) {
+  //   const src = elements[i].dataset.setbg;
+  //   elements[i].style.backgroundImage = `url('${src}')`;
+  // }
 };
 
 bgElements();
